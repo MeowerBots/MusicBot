e866e631752a4b1a339803f6f42d1ce6a4c5596d
M.D. Walters <808426@pdsb.net>
2022-11-22 16:52:31 -0500

    Introducing... Our brand new explicit song filter!

--- musixmatch.js
+++ musixmatch.js
@@ -12,6 +12,13 @@ export default class Musixmatch {
         if (tracks.message.body.track_list.length == 0) {
             return "";
         } else {
+            var list = tracks.message.body.track_list;
+            for (let i in list) {
+                if (list[i].track.explicit === 1) {
+                    list.splice(i, 1);
+                }
+            }
+            tracks.message.body.track_list = list;
             return tracks.message.body.track_list;
         }
     }
@@ -20,7 +27,7 @@ export default class Musixmatch {
         var url = encodeURIComponent(`https://api.musixmatch.com/ws/1.1/track.lyrics.get?commontrack_id=${id}&apikey=${this.key}`);
         var track = await fetch(`https://api.allorigins.win/get?url=${url}`).then(res => res.json());
         var lyrics = JSON.parse(track.contents);
-        if (lyrics.message.header.status_code != 200 || lyrics.message.body.lyrics.explicit === 1) {
+        if (lyrics.message.header.status_code != 200 || song.message.body.track.explicit === 1) {
             return "";
         } else {
             return lyrics.message.body.lyrics.lyrics_body;
